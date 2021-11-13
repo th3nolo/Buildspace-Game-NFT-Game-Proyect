faece710911076ecc5a2aacc99edd3f4a6e1be2e
Manuel Parra <>
2021-11-13 16:28:45 +0000

    add Select Character component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,15 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 import SelectCharacter from './Components/SelectCharacter';
-import { CONTRACT_ADDRESS, transformCharacterData } from './constants';
-import myEpicGame from './utils/MyEpicGame.json';
-import { ethers } from 'ethers';
+
 
 // Constants
-const TWITTER_HANDLE = 'th3nolo';
+const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-
 
 const App = () => {
   // State
   const [currentAccount, setCurrentAccount] = useState(null);
-  
-  const [characterNFT, setCharacterNFT] = useState(null);
 
   // Actions
   const checkIfWalletIsConnected = async () => {
@@ -73,92 +68,44 @@ const App = () => {
   };
 
   useEffect(() => {
-    checkIfWalletIsConnected();  
-
-  /*
-   * We only want to run this, if we have a connected wallet
-   */
-  if (currentAccount) {
-    console.log('CurrentAccount:', currentAccount);
-    fetchNFTMetadata();
-  }
-}, [currentAccount]);
+    checkIfWalletIsConnected();
   }, []);
 
-
-useEffect(() => {
-    const fetchNFTMetadata = async () => {
-    console.log('Checking for Character NFT on address:', currentAccount);
-
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    const gameContract = new ethers.Contract(
-      CONTRACT_ADDRESS,
-      myEpicGame.abi,
-      signer
-    );
-
-    const txn = await gameContract.checkIfUserHasNFT();
-    if (txn.name) {
-      console.log('User has character NFT');
-      setCharacterNFT(transformCharacterData(txn));
-    } else {
-      console.log('No character NFT found');
-    }
-  };
-  
-// Render Methods
-const renderContent = () => {
-  /*
-   * Scenario #1*/  
-  if (!currentAccount) {
-    return (
-      <div className="connect-wallet-container">
-        <img
-          src="https://i.pinimg.com/originals/6b/1a/ee/6b1aee44d51b4472ce60f920a8992c37.gif"
-          alt="Monty Python Gif"
-        />
-        <button
-          className="cta-button connect-wallet-button"
-          onClick={connectWalletAction}
-        >
-          Connect Wallet To Get Started
-        </button>
-      </div>
-    );
-    /*
-     * Scenario #2
-     */
-  } else if (currentAccount && !characterNFT) {
-    return <SelectCharacter setCharacterNFT={setCharacterNFT} />;
-  }
-};
-
-
-
   return (
-  <div className="App">
-    <div className="container">
-      <div className="header-container">
-        <p className="header gradient-text">⚔️  Beer Metaverse ⚔️</p>
-        <p className="sub-text">Team up to protect the Metaverse!</p>
-        {/* This is where our button and image code used to be!
-         *	Remember we moved it into the render method.
-         */}
-        {renderContent()}
-      </div>
-      <div className="footer-container">
-        <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-        <a
-          className="footer-text"
-          href={TWITTER_LINK}
-          target="_blank"
-          rel="noreferrer"
-        >{`built with @${TWITTER_HANDLE}`}</a>
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
+          <p className="sub-text">Team up to protect the Metaverse!</p>
+          <div className="connect-wallet-container">
+            <img
+              src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
+              alt="Monty Python Gif"
+            />
+            {/*
+             * Button that we will use to trigger wallet connect
+             * Don't forget to add the onClick event to call your method!
+             */}
+            <button
+              className="cta-button connect-wallet-button"
+              onClick={connectWalletAction}
+            >
+              Connect Wallet To Get Started
+            </button>
+          </div>
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built with @${TWITTER_HANDLE}`}</a>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
 };
 
 export default App;
