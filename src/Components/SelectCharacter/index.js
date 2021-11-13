1c8bd5ee61e737a408fd37e3ca1d1f8ebf7f8bf9
Manuel Parra <>
2021-11-13 17:10:14 +0000

    Setting a reusable etherjs object

--- src/Components/SelectCharacter/index.js
+++ src/Components/SelectCharacter/index.js
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import './SelectCharacter.css';
+import { ethers } from 'ethers';
+import { CONTRACT_ADDRESS, transformCharacterData } from '../../constants';
+import myEpicGame from '../../utils/MyEpicGame.json';
 
 /*
  * Don't worry about setCharacterNFT just yet, we will talk about it soon!
  */
 const SelectCharacter = ({ setCharacterNFT }) => {
+  const [characters, setCharacters] = useState([]);
+  const [gameContract, setGameContract] = useState(null);
+
   return (
     <div className="select-character-container">
       <h2>Mint Your Hero. Choose wisely.</h2>
