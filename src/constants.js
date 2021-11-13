cf31b39d21318a914c8ffdf2289e24dc0be01762
ManuelParra1 <>
2021-11-13 16:25:37 +0000

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0xdb324EE4ab2B5D6e546d146455C3dc63Aaa8Fc8B';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
